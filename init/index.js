b650787c608d9b9ec75ae1d42203fd701cf648ee
rishiisoni <therishisoni13@gmail.com>
2024-10-22 01:08:07 +0530

    second commit

--- init/index.js
+++ init/index.js
@@ -1,21 +1,31 @@
+require('dotenv').config({ path : "../.env"});  
+
 const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Listing = require('../models/listing.js');
 
+const dbUrl = process.env.ATLASDB_URL;
+
+console.log('MongoDB URI:', dbUrl);  
+
 main()
-    .then(() => {console.log("Connected to server")})
-    .catch(err => console.log(err));
+    .then(() => {
+        console.log("Connected to MongoDB");
+        initDB(); 
+    })
+    .catch(err => console.error("Error connecting to MongoDB:", err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/airbnb');
+    await mongoose.connect(dbUrl);  
 }
-
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "66fce7febb9fcfe9814a4367"}));
-    await Listing.insertMany(initData.data);
-    console.log("Data was initialized");
-}
-
-initDB();
+    try {
+        await Listing.deleteMany({});
+        initData.data = initData.data.map((obj) => ({ ...obj, owner: "6715e6207d3c6a092b90d546" }));
+        await Listing.insertMany(initData.data);
+        console.log("Data was initialized");
+    } catch (error) {
+        console.error("Error initializing data:", error);
+    }
+};
